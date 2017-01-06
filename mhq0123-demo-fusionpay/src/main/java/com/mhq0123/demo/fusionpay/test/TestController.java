7feb6954269f6c7e7abb02958349743bec16e7a0
mhq0123 <532386274@qq.com>
2017-01-06 18:24:01 +0800

    初始化demo工程

--- mhq0123-demo-fusionpay/src/main/java/com/mhq0123/demo/fusionpay/test/TestController.java
+++ mhq0123-demo-fusionpay/src/main/java/com/mhq0123/demo/fusionpay/test/TestController.java
@@ -1,7 +1,11 @@
 package com.mhq0123.demo.fusionpay.test;
 
+import com.mhq0123.demo.fusionpay.charges.ChargesPath;
+import com.mhq0123.demo.fusionpay.charges.bean.Charges;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * project: mhq0123-demo
@@ -15,5 +19,15 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "/test/test";
+    }
+
+    /**
+     * 进入渠道选择页面
+     * @return
+     */
+    @GetMapping(ChargesPath.BASE + ChargesPath.CHARGES_PC_CNCB_ALIPAY)
+    public String chargesPcCncbAlipay(@ModelAttribute("charges")Charges charges) {
+
+        return ChargesPath.BASE + ChargesPath.CHARGES_PC_CNCB_ALIPAY;
     }
 }
